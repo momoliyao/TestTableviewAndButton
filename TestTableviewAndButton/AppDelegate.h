177a42c66786c704f63a06e35c0c21869924295d
momoliyao <1778775543@qq.com>
2017-02-24 15:12:36 +0800

    Initial Commit

---
+++ TestTableviewAndButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestTableviewAndButton
+//
+//  Created by yinyan on 2017/2/24.
+//  Copyright © 2017年 yinyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
